242b30c664a0156b5012d099861a2e1fc079a318
Cris Herrera <crherrer54@alumnes.ub.edu>
2026-04-28 13:27:52 +0200

    llista

--- src/prog2/model/Llista.java
+++ src/prog2/model/Llista.java
@@ -20,42 +20,46 @@ public class Llista<T> implements Serializable {
      * Retornar nombre d'elements continguts a la llista
      */
     public int getSize() {
-          // TO-BE-DONE
+        return llista.size();
     }
 
     /**
      * Afegir element a la llista. Afegeix l'element t a la llista
      */
     public void afegir(T t) throws BiblioException {
-          // TO-BE-DONE
+        if (t==null) {
+            throw new BiblioException("L'element no pot ser null.");
+        } else {
+            llista.add(t);
+        }
     }
 
     /**
      * Esborrar element de la llista. Esborra l'element t a la llista
      */
     public void esborrar(T t) {
-          // TO-BE-DONE
+        llista.remove(t);
     }
 
     /**
      * Retornar element de la llista a la posició position
      */
     public T getAt(int position) {
-          // TO-BE-DONE
+        return llista.get(position);
     }
 
     /**
      * Buidar tots el elements de la llista
      */
     public void clear() {
-          // TO-BE-DONE
+        llista.clear();
     }
 
     /**
      * Retornar true si la llista és buida
      */
     public boolean isEmpty() {
-          // TO-BE-DONE
+        return llista.isEmpty();
     }
 
     /**
---
+++ src/prog2/model/LlistaPrestecs.java
@@ -0,0 +1,9 @@
+package prog2.model;
+
+import java.io.Serializable;
+
+public class LlistaPrestecs extends Llista<Prestec> implements Serializable {
+    public LlistaPrestecs() {
+        super();
+    }
+}
