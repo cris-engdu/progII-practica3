b8c5841f7eb54bf90c9d28cf1fc637f7b4a07227
jordiguerreroo <jguerrpa119@alumnes.ub.edu>
2026-05-05 13:38:40 +0200

    iniciadorbiblio

--- src/prog2/vista/IniciadorBiblioUb.java
+++ src/prog2/vista/IniciadorBiblioUb.java
@@ -1,4 +1,13 @@
 package prog2.vista;
 
 public class IniciadorBiblioUb {
+    private BiblioUB biblio;
+
+    public IniciadorBiblioUb() {
+        biblio=new BiblioUB();
+        biblio.gestioBiblioUB();
+
+
+    }
+
 }
